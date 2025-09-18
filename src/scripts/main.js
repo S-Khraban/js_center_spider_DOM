bd89c383bfb6ba03a2c35de7f3bde1d442fa19d6
S-Khraban <s.hraban@i.ua>
2025-09-18 16:44:56 +0300

    _Solution for spider center_

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
-'use strict';
+(function centerSpider() {
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
 
-// write code here
+  if (!wall || !spider) {
+    return;
+  }
+
+  wall.style.position = wall.style.position || 'relative';
+  spider.style.position = 'absolute';
+
+  const place = () => {
+    const leftPos = (wall.clientWidth - spider.offsetWidth) / 2;
+    const topPos = (wall.clientHeight - spider.offsetHeight) / 2;
+
+    spider.style.left = `${leftPos}px`;
+    spider.style.top = `${topPos}px`;
+  };
+
+  if (spider.complete) {
+    place();
+  } else {
+    spider.addEventListener('load', place, { once: true });
+  }
+
+  window.addEventListener('resize', place);
+})();
